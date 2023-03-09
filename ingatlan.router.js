4ed494bf97939abea396cde30ff60524b90fcf77
Lali98 <kisslajosbertalan77@gmail.com>
2023-03-09 12:32:00 +0100

    2022.03.09

--- ingatlan.router.js
+++ ingatlan.router.js
@@ -5,8 +5,8 @@ const Kategoria = db.Kategoria;
 const bodyParser = require('body-parser');
 
 const routes = express.Router();
-express().use(bodyParser.json());
 
+// Get all ingatlan
 routes.get('/ingatlan', (req, res) => {
     Ingatlan.findAll({include: Kategoria})
         .then(data => {
@@ -14,8 +14,36 @@ routes.get('/ingatlan', (req, res) => {
         })
 });
 
-routes.post('/ingatlan', (req, res) => {
-    
+// Post ingatlan
+routes.post('/ingatlan', bodyParser.json(), (req, res) => {
+    if(!req.body.kategoria || !req.body.tehermentes || !req.body.ar) {
+        res.status(400).send('Hiányos adatok.');
+        return;
+    }
+    const newData = {
+        kategoria: req.body.kategoria,
+        leiras: req.body.leiras ? req.body.leiras : null,
+        hirdetesDatuma: req.body.hirdetesDatuma ? req.body.hirdetesDatuma : null,
+        tehermentes: req.body.tehermentes,
+        ar: req.body.ar,
+        kepUrl: req.body.kepUrl ? req.body.kepUrl : null
+    };
+    Ingatlan.create(newData)
+        .then(data => {res.send({id: data.id}).status(201)})
+        .catch(err => {res.status(500).send('Szerver oldali hiba!' + err)});
+});
+
+routes.delete('/ingatlan/:id', (req, res) => {
+    const id = req.params.id;
+    Ingatlan.destroy({where: {id: id}})
+        .then(num => {
+            if (num === 1) {
+                res.status(204).send();
+            } else {
+                res.send('Az ingatlan nem létezik.').status(404);
+            }
+        })
+        .catch(err => {res.status(500).send('Szerver oldali hiba!' + err)});
 })
 
 module.exports = routes;
